0c8be462197d2fe6ac3212e50819823699131295
ty83zp80 <ty83zp80@gmail.com>
2020-08-20 18:21:17 +0800

    release key

--- src/com/bimo/tank/TankFrame.java
+++ src/com/bimo/tank/TankFrame.java
@@ -30,7 +30,7 @@ public class TankFrame extends Frame {
 	}
 	
 	class MyKeyListener extends KeyAdapter{
-
+		boolean bU =false,bR= false, bD = false, bL = false;
 		@Override
 		public void keyPressed(KeyEvent e) {
 			//x += 200;
@@ -38,15 +38,22 @@ public class TankFrame extends Frame {
 			switch(key) {
 				case KeyEvent.VK_LEFT: 
 					x-=5;
+					bL = true;
+					if(bU == true) {
+						y -= 5;
+					}
 					break;
 				case KeyEvent.VK_RIGHT:
 					x+=5;
+					bR = true;
 					break;
 				case KeyEvent.VK_UP:
 					y -= 5;
+					bU = true;
 					break;
 				case KeyEvent.VK_DOWN:
 					y+=5;
+					bD = true;
 					break;
 				default:
 					break;
@@ -55,7 +62,23 @@ public class TankFrame extends Frame {
 
 		@Override
 		public void keyReleased(KeyEvent e) {
-			
+			int key = e.getKeyCode();
+			switch(key) {
+				case KeyEvent.VK_LEFT: 
+					bL = false;
+					break;
+				case KeyEvent.VK_RIGHT:
+					bR = false;
+					break;
+				case KeyEvent.VK_UP:
+					bU = false;
+					break;
+				case KeyEvent.VK_DOWN:
+					bD = false;
+					break;
+				default:
+					break;
+			}
 		}
 		
 	}
